87d8e9c30d29cd1a78032de1f0cf6d35e2a30196
TK <tawheed@gmail.com>
2021-02-02 17:27:39 -0600

    Tweaked name.

--- src/components/SundayHeader.js
+++ src/components/SundayHeader.js
@@ -15,7 +15,7 @@ export class SundayHeader extends Component {
               
               <img className="logo" src="https://www.getunstoppable.com/wp-content/uploads/2018/02/ICON@1x.jpg" alt="TK Kader Unstoppable Logo"></img> 
 
-              <p className="title">Unstoppable</p>
+              <p className="title">Unstoppable Flow</p>
 
               <p className="small"><strong>Get into an instant flow state with the power of the Pomodoro technique and Alpha Waves playing in the background.</strong></p>
 
